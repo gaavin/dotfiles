4af4a36723c7d0bf20a9c48adba4977f2d3a4ad4
Max Power <me@gavinpower.dev>
2026-08-03 15:41:56 -0230

    try to fix audio crackling in minecraft

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -154,21 +154,21 @@
       extraConfig.pipewire."92-low-latency" = {
         "context.properties" = {
           "default.clock.rate" = 48000;
-          "default.clock.quantum" = 64;
-          "default.clock.min-quantum" = 64;
-          "default.clock.max-quantum" = 64;
+          "default.clock.quantum" = 128;
+          "default.clock.min-quantum" = 128;
+          "default.clock.max-quantum" = 128;
         };
       };
       extraConfig.pipewire-pulse."92-low-latency" = {
         "pulse.properties" = {
-          "pulse.min.req" = "64/48000";
-          "pulse.default.req" = "64/48000";
-          "pulse.max.req" = "64/48000";
-          "pulse.min.quantum" = "64/48000";
-          "pulse.max.quantum" = "64/48000";
+          "pulse.min.req" = "128/48000";
+          "pulse.default.req" = "128/48000";
+          "pulse.max.req" = "128/48000";
+          "pulse.min.quantum" = "128/48000";
+          "pulse.max.quantum" = "128/48000";
         };
         "stream.properties" = {
-          "node.latency" = "64/48000";
+          "node.latency" = "128/48000";
           "resample.quality" = 1;
         };
       };
